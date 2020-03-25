d6370a814302f6142438f0776df84e788ca5edd9
miluku1018 <miluku1018@gmail.com>
2020-03-26 00:06:22 +0800

    update community& add post 2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,27 @@
 class PostsController < ApplicationController
   def index
-
+    @psots = Post.all 
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
-
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to psots_path, notice: "Create post successfully"
+    else
+      render :new
+    end
   end
 
   def show
-
+ 
+  end
+ 
+  def post_params
+    params.require(:post).permit(:title, :body, :upvotes, :downvotes)
   end
 
 end
